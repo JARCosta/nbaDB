b4970264fba51f620f1a3aca6efb35660639d366
João Costa <joaoarcosta@tecnico.ulisboa.pt>
2023-03-21 17:17:48 +0000

    fixed serverImpl imports

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import hashlib
 import time
 from flask import Flask, request, session
 
-import app2
+import serverImpl
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -19,39 +19,39 @@ def before_request():
 
 @app.route("/")
 def root():
-    return app2.root()
+    return serverImpl.root()
 
 @app.route("/teams")
 def teams():
-    return app2.teams()
+    return serverImpl.teams()
 
 @app.route("/update_teams")
 def update_games():
-    return app2.update_games()
+    return serverImpl.update_games()
 
 @app.route("/players")
 def players():
-    return app2.players()
+    return serverImpl.players()
 
 @app.route("/update_players")
 def update_players():
-    return app2.update_players()
+    return serverImpl.update_players()
 
 @app.route("/games")
 def games():
-    return app2.games()
+    return serverImpl.games()
 
 @app.route("/update_games")
 def update_games():
-    return app2.update_games
+    return serverImpl.update_games
 
 @app.route("/show_games")
 def show_games():
-    return app2.show_games()
+    return serverImpl.show_games()
 
 @app.route("/clear")
 def clear():
-    return app2.clear()
+    return serverImpl.clear()
 
 
 if __name__ == '__main__':
